a9a0a3e1213674435482d874f99796fec3a2dca7
Mirko <64952483+Mirko-r@users.noreply.github.com>
2021-05-28 12:54:49 +0200

    Added progress bar

--- extractor/extractor.sh
+++ extractor/extractor.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 # Mirko Rovere
 
+prog() {
+    local w=80 p=$1;  shift
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /.};
+    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*"; 
+}
+
+print() {
+    echo ""
+    for x in {1..100} ; do
+        prog "$x" extraction "${args[i]}" in progress
+        sleep .02
+    done
+    echo ""
+}
+
 if [ $# -lt 1 ]; then
     echo "Usage: extract /path/to/file1 /path/to/file2 ..."
     exit 1
@@ -12,26 +27,39 @@ set ${!args[@]}
 for i ; do
     if [ "${args[i]}" ]; then
         echo ""
-        echo "::extraction '${args[i]}' in prograss..."
-        echo ""
         case "${args[i]}" in
-            *.tar.xz)   tar -xvf "${args[i]}"                          ;;
-            *.tar.bz2)  tar -jxvf "${args[i]}"                         ;;
-            *.tar.gz)   tar -zxvf "${args[i]}"                         ;;
-            *.bz2)      bunzip2 "${args[i]}"                           ;;
-            *.dmg)      hdiutil mount "${args[i]}"                    ;;
-            *.gz)       gunzip "${args[i]}"                            ;;
-            *.tar)      tar -xvf "${args[i]}"                          ;;
-            *.tbz2)     tar -jxvf "${args[i]}"                         ;;
-            *.tgz)      tar -zxvf "${args[i]}"                         ;;
-            *.zip)      unzip "${args[i]}"                             ;;
-            *.pax)      cat "${args[i]}"  | pax -r                     ;;
-            *.pax.z)    uncompress "${args[i]}"  --stdout | pax -r     ;;
-            *.rar)      7z x "${args[i]}"                              ;;
-            *.z)        uncompress "${args[i]}"                        ;;
-            *.7z)       7z x "${args[i]}"                              ;;
-            *)          echo "'${args[i]}' cannot be extracted" ;;
-    esac
+            *.tar.xz)   tar -xvf "${args[i]}"                          ;
+                        print                                          ;;
+            *.tar.bz2)  tar -jxvf "${args[i]}"                         ;
+                        print                                          ;;
+            *.tar.gz)   tar -zxvf "${args[i]}"                         ;
+                        print                                          ;;
+            *.bz2)      bunzip2 "${args[i]}"                           ;
+                        print                                          ;;
+            *.dmg)      hdiutil mount "${args[i]}"                     ;
+                        print                                          ;;
+            *.gz)       gunzip "${args[i]}"                            ;
+                        print                                          ;;
+            *.tar)      tar -xvf "${args[i]}"                          ;
+                        print 
+            *.tbz2)     tar -jxvf "${args[i]}"                         ;
+                        print                                          ;;
+            *.tgz)      tar -zxvf "${args[i]}"                         ;
+                        print                                          ;;
+            *.zip)      unzip "${args[i]}"                             ;
+                        print                                          ;;
+            *.pax)      cat "${args[i]}"  | pax -r                     ;
+                        print                                          ;;
+            *.pax.z)    uncompress "${args[i]}"  --stdout | pax -r     ;
+                        print                                          ;;
+            *.rar)      7z x "${args[i]}"                              ;
+                        print                                          ;;
+            *.z)        uncompress "${args[i]}"                        ;
+                        print                                          ;;
+            *.7z)       7z x "${args[i]}"                              ;
+                        print                                          ;;
+            *)          echo "'${args[i]}' cannot be extracted"        ;;
+        esac
     else
         echo "'${args[i]}' not a valid file"
     fi
