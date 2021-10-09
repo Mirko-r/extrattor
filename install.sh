64cd5fb9c7405843a8bb5914db038fe37e1c3dd5
Mirko Rovere <64952483+Mirko-r@users.noreply.github.com>
2021-10-09 09:22:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Installing in progress"
-cp extractor/extractor.sh /usr/bin/extractor
-chmod 755 /usr/bin/extractor
+cp extractor/extractor.sh /usr/bin/extrattor
+chmod 755 /usr/bin/extrattor
 echo "Done"
